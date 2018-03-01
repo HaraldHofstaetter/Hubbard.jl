9655234d753cde8bd415c8659d2d4a533a30de8f
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2018-03-01 17:17:22 +0100

    made everything more general

--- examples/time_stepper.jl
+++ examples/time_stepper.jl
@@ -4,6 +4,10 @@ import FExpokit: get_lwsp_liwsp_expv
 
 get_lwsp_liwsp_expv(H, scheme, m::Integer=30) = get_lwsp_liwsp_expv(size(H, 2), m)
 
+const for_expv = 0
+const for_Gamma = 1
+const for_Gamma_d = 2
+const for_Gamma_d_symmetrized = 3
 
 struct CommutatorFreeScheme
     A::Array{Float64,2}
@@ -12,6 +16,7 @@ struct CommutatorFreeScheme
 end
 
 get_order(scheme::CommutatorFreeScheme) = scheme.p
+number_of_exponentials(scheme::CommutatorFreeScheme) = size(scheme.A, 1)
 
 CF2 = CommutatorFreeScheme( ones(1,1), [1/2], 2 )
 
@@ -38,160 +43,311 @@ CF6 = CommutatorFreeScheme(
   [1/2-sqrt(15)/10, 1/2, 1/2+sqrt(15)/10],
   6)
 
+function prepare_Omega(H, j::Int, f::Function, which::Int, t::Float64, dt::Float64, scheme::CommutatorFreeScheme)
+    set_matrix_times_minus_i!(H, which!=for_expv) 
+    if which==for_Gamma_d
+       g = 0.0
+       f1 = sum(scheme.c.*scheme.A[j,:].*f.(t+dt*scheme.c))
+    elseif which==for_Gamma_d_symmetrized
+       g = 0.0
+       f1 = sum((scheme.c-0.5).*scheme.A[j,:].*f.(t+dt*scheme.c))
+    else
+       g = sum(scheme.A[j,:])
+       f1 = sum(scheme.A[j,:].*f.(t+dt*scheme.c)) 
+    end   
+    (g, f1)
+end
+
+function Omega!(w::Array{Complex{Float64},1}, v::Array{Complex{Float64},1}, H, args::Tuple,
+                p::Int, scheme::CommutatorFreeScheme)
+    g, f1 = args           
+    set_fac!(H, g, f1)
+    A_mul_B!(w, H, v) 
+end
+
+abstract type Magnus2 end
+
+get_order(::Type{Magnus2}) = 2
+number_of_exponentials(::Type{Magnus2}) = 1
+
+function prepare_Omega(H, j::Int, f::Function, which::Int, t::Float64, dt::Float64, scheme::Type{Magnus2})
+    set_matrix_times_minus_i!(H, which!=for_expv) 
+    g = which==for_Gamma_d?0.0:1.0 
+    f1 = f(t+0.5*dt)
+    (g, f1)
+end
+
+function Omega!(w::Array{Complex{Float64},1}, v::Array{Complex{Float64},1}, H, args::Tuple,
+                p::Int, ::Type{Magnus2})
+    g, f1 = args
+    set_fac!(H, g, f1)
+    A_mul_B!(w, H, v)
+end
+
+
+abstract type Magnus4 end
+
+function get_lwsp_liwsp_expv(H, scheme::Type{Magnus4}, m::Integer=30) 
+    (lw, liw) = get_lwsp_liwsp_expv(size(H, 2), m)
+    (lw+size(H, 2), liw)
+end
+
+get_order(::Type{Magnus4}) = 4
+number_of_exponentials(::Type{Magnus4}) = 1
+
+function prepare_Omega(H, j::Int, f::Function, which::Int, t::Float64, dt::Float64, scheme::Type{Magnus4})
+    set_matrix_times_minus_i!(H, which!=for_expv) 
+    c = dt*sqrt(3)/12*1im
+    g = which==for_Gamma_d?0.0:1.0 
+    f1 = f(t+dt*(1/2-sqrt(3)/6))
+    f2 = f(t+dt*(1/2+sqrt(3)/6))
+    n = size(H, 2)
+    s = unsafe_wrap(Array, pointer(get_wsp(H), length(get_wsp(H))-n+1), n, false)
+    (c, g, f1, f2, s)
+end
+
+function Omega!(w::Array{Complex{Float64},1}, v::Array{Complex{Float64},1}, H, args::Tuple,
+                p::Int, ::Type{Magnus4})
+    c, g, f1, f2, s = args
+
+    set_fac!(H, g, f1)
+    A_mul_B!(s, H, v)
+
+    w[:] = 0.5*s[:]
+    
+    set_fac!(H, g, f2)
+    A_mul_B!(s, H, s)
+    
+    w[:] -= c*s
+
+    set_fac!(H, g, f2)
+    A_mul_B!(s, H, v)
+
+    w[:] += 0.5*s
+
+    set_fac!(H, g, f1)
+    A_mul_B!(s, H, s)
+    
+    w[:] += c*s
+end
+
 
 function step!(psi::Array{Complex{Float64},1}, H, f::Function, 
-               t::Real, dt::Real, scheme::CommutatorFreeScheme)
+               t::Real, dt::Real, scheme)
     state = save_state(H)
-
-    set_matrix_times_minus_i!(H, false) # this is done by expv
-    for j=1:size(scheme.A, 1)
-        set_fac!(H, sum(scheme.A[j,:]), sum(scheme.A[j,:].*f.(t+dt*scheme.c)))
-
-        expv!(psi, dt, H, psi, anorm=get_norm0(H), 
+    p = get_order(scheme)
+    for j=1:number_of_exponentials(scheme)
+        args = prepare_Omega(H, j, f, for_expv, t, dt, scheme)
+        expv!(psi, dt, Omega!, psi, get_norm0(H),
+              args = (H, args, p, scheme),
               matrix_times_minus_i=true, hermitian=true,
               wsp=get_wsp(H), iwsp=get_iwsp(H))
-
     end
-
     restore_state!(H, state)
 end    
 
-
 function Gamma!(r::Vector{Complex{Float64}}, H,
                 u::Vector{Complex{Float64}}, p::Int, dt::Float64, 
-                g::Float64, f::Complex{Float64}, fd::Complex{Float64},
-                s1::Vector{Complex{Float64}}, s2::Vector{Complex{Float64}})
-    
+                args::Tuple, argsd::Tuple,
+                s1::Vector{Complex{Float64}}, s2::Vector{Complex{Float64}}, scheme)
     #s2=B*u
-    set_fac!(H, g, f);  A_mul_B!(s2, H, u)
+    Omega!(s2, u, H, args, p, scheme)
     r[:] = s2[:]    
     
     if p>=1
         #s1=A*u
-        set_fac!(H, 0.0, fd);  A_mul_B!(s1, H, u)
+        Omega!(s1, u, H, argsd, p, scheme)
         r[:] += dt*s1[:] 
     end
     if p>=2
         #s1=B*s1=BAu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^2/2)*s1[:] 
     end
     if p>=3
         #s1=B*s1=BBAu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^3/6)*s1[:] 
     end
     if p>=4
         #s1=B*s1=BBBAu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^4/24)*s1[:] 
     end
     if p>=5
         #s1=B*s1=BBBBAu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^5/120)*s1[:] 
     end
     if p>=6
         #s1=B*s1=BBBBBAu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^6/720)*s1[:] 
     end
 
     if p>=2
         #s1=A*s2=ABu
-        set_fac!(H, 0.0, fd);  A_mul_B!(s1, H, s2)
+        Omega!(s1, s2, H, argsd, p, scheme)
         r[:] -= (dt^2/2)*s1[:] 
     end
     if p>=3
         #s1=B*s1=BABu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] -= (dt^3/3)*s1[:] 
     end
     if p>=4
         #s1=B*s1=BBABu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] -= (dt^4/8)*s1[:] 
     end
     if p>=5
         #s1=B*s1=BBBABu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] -= (dt^5/30)*s1[:] 
     end
     if p>=6
         #s1=B*s1=BBBBABu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] -= (dt^6/144)*s1[:] 
     end
 
     if p>=3
         #s2=B*s2=BBu
-        set_fac!(H, g, f);  A_mul_B!(s2, H, s2)
+        Omega!(s2, s2, H, args, p, scheme)
         #s1=A*s2=ABBu
-        set_fac!(H, 0.0, fd);  A_mul_B!(s1, H, s2)
+        Omega!(s1, s2, H, argsd, p, scheme)
         r[:] += (dt^3/6)*s1
     end
     if p>=4
         #s1=B*s1=BABBu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^4/8)*s1
     end
     if p>=5
         #s1=B*s1=BBABBu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^5/20)*s1
     end
     if p>=6
         #s1=B*s1=BBBABBu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^6/72)*s1
     end
 
     if p>=4
         #s2=B*s2=BBBu
-        set_fac!(H, g, f);  A_mul_B!(s2, H, s2)
+        Omega!(s2, s2, H, args, p, scheme)
         #s1=A*s2=ABBBu
-        set_fac!(H, 0.0, fd);  A_mul_B!(s1, H, s2)
+        Omega!(s1, s2, H, argsd, p, scheme)
         r[:] -= (dt^4/24)*s1
     end
     if p>=5
         #s1=B*s1=BABBBu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] -= (dt^5/30)*s1
     end
     if p>=6
         #s1=B*s1=BBABBBu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] -= (dt^6/72)*s1
     end
 
     if p>=5
         #s2=B*s2=BBBBu
-        set_fac!(H, g, f);  A_mul_B!(s2, H, s2)
+        Omega!(s2, s2, H, args, p, scheme)
         #s1=A*s2=ABBBBu
-        set_fac!(H, 0.0, fd);  A_mul_B!(s1, H, s2)
+        Omega!(s1, s2, H, argsd, p, scheme)
         r[:] += (dt^5/120)*s1
     end
     if p>=6
         #s1=B*s1=BABBBBu
-        set_fac!(H, g, f);  A_mul_B!(s1, H, s1)
+        Omega!(s1, s1, H, args, p, scheme)
         r[:] += (dt^6/144)*s1
     end
 
     if p>=6
         #s2=B*s2=BBBBBu
-        set_fac!(H, g, f);  A_mul_B!(s2, H, s2)
+        Omega!(s2, s2, H, args, p, scheme)
         #s1=A*s2=ABBBBBu
-        set_fac!(H, 0.0, fd);  A_mul_B!(s1, H, s2)
+        Omega!(s1, s2, H, argsd, p, scheme)
         r[:] -= (dt^6/720)*s1
     end
 end
 
-abstract type Magnus2 end
 
-struct Magnus2Exponent
-    H
-    f::Complex{Float64}
+function step_estimated!(psi::Array{Complex{Float64},1}, psi_est::Array{Complex{Float64},1},
+                 H, f::Function, fd::Function, t::Real, dt::Real,
+                 scheme::CommutatorFreeScheme;
+                 symmetrized_defect::Bool=false)
+    state = save_state(H)
+
+    p = get_order(scheme)
+    for_Gamma_d1 = symmetrized_defect?for_Gamma_d_symmetrized:for_Gamma_d
+
+    n = size(H, 2)
+    s = unsafe_wrap(Array, pointer(get_wsp(H), 1), n, false)
+    s1 = unsafe_wrap(Array, pointer(get_wsp(H), n+1),   n, false)
+    s2 = unsafe_wrap(Array, pointer(get_wsp(H), 2*n+1), n, false)
+
+    if symmetrized_defect
+        set_fac!(H, -0.5, -0.5*f(t))
+        set_matrix_times_minus_i!(H, true) 
+        LinAlg.A_mul_B!(psi_est, H, psi)
+
+        args = prepare_Omega(H, 1, f, for_expv, t, dt, scheme)
+        expv!(psi_est, dt, Omega!, psi_est, get_norm0(H), 
+              args = (H, args, p, scheme),
+              matrix_times_minus_i=true, hermitian=true,
+              wsp=get_wsp(H), iwsp=get_iwsp(H)) 
+    else
+        psi_est[:] = 0.0
+    end
+
+    args = prepare_Omega(H, 1, f, for_expv, t, dt, scheme)
+    expv!(psi, dt, Omega!, psi, get_norm0(H), 
+          args = (H, args, p, scheme),
+          matrix_times_minus_i=true, hermitian=true,
+          wsp=get_wsp(H), iwsp=get_iwsp(H))
+
+    args  = prepare_Omega(H, 1, f,  for_Gamma,    t, dt, scheme)
+    argsd = prepare_Omega(H, 1, fd, for_Gamma_d1, t, dt, scheme)
+    Gamma!(s, H, psi, p, dt, args, argsd, s1, s2, scheme)
+    psi_est[:] += s[:]
+
+    for j=2:size(scheme.A, 1)
+
+        args = prepare_Omega(H, j, f, for_expv, t, dt, scheme)
+        expv!(psi_est, dt, Omega!, psi_est, get_norm0(H), 
+              args = (H, args, p, scheme),
+              matrix_times_minus_i=true, hermitian=true,
+              wsp=get_wsp(H), iwsp=get_iwsp(H))
+
+        args = prepare_Omega(H, j, f, for_expv, t, dt, scheme)
+        expv!(psi, dt, Omega!, psi, get_norm0(H), 
+              args = (H, args, p, scheme),
+              matrix_times_minus_i=true, hermitian=true,
+              wsp=get_wsp(H), iwsp=get_iwsp(H))
+
+        args  = prepare_Omega(H, j, f,  for_Gamma,    t, dt, scheme)
+        argsd = prepare_Omega(H, j, fd, for_Gamma_d1, t, dt, scheme)
+        Gamma!(s, H, psi, p, dt, args, argsd, s1, s2, scheme)
+        psi_est[:] += s[:]
+
+    end
+   
+    if symmetrized_defect
+        set_fac!(H, 0.5, 0.5*f(t+dt))
+    else
+        set_fac!(H, 1.0, f(t+dt))
+    end
+    LinAlg.A_mul_B!(s, H, psi)
+    psi_est[:] -= s[:]
+
+    psi_est[:] *= dt/(p+1)
+
+    restore_state!(H, state)
 end
+
 
 function step_estimated!(psi::Array{Complex{Float64},1}, psi_est::Array{Complex{Float64},1},
                  H, f::Function, fd::Function, t::Real, dt::Real,
@@ -234,197 +390,6 @@ function step_estimated!(psi::Array{Complex{Float64},1}, psi_est::Array{Complex{
     
     restore_state!(H, state)
 end
-
-function step_estimated!(psi::Array{Complex{Float64},1}, psi_est::Array{Complex{Float64},1},
-                 H, f::Function, fd::Function, t::Real, dt::Real,
-                 scheme::CommutatorFreeScheme;
-                 symmetrized_defect::Bool=false)
-    state = save_state(H)
-
-    n = size(H, 2)
-    s = unsafe_wrap(Array, pointer(get_wsp(H), 1), n, false)
-    s1 = unsafe_wrap(Array, pointer(get_wsp(H), n+1),   n, false)
-    s2 = unsafe_wrap(Array, pointer(get_wsp(H), 2*n+1), n, false)
-
-    if symmetrized_defect
-        set_fac!(H, -0.5, -0.5*f(t))
-        set_matrix_times_minus_i!(H, true) 
-        LinAlg.A_mul_B!(psi_est, H, psi)
-
-        set_fac!(H, sum(scheme.A[1,:]), sum(scheme.A[1,:].*f.(t+dt*scheme.c)))
-        set_matrix_times_minus_i!(H, false) # this is done by expv
-        expv!(psi_est, dt, H, psi_est, anorm=get_norm0(H), 
-              matrix_times_minus_i=true, hermitian=true,
-              wsp=get_wsp(H), iwsp=get_iwsp(H)) 
-        set_matrix_times_minus_i!(H, true)
-    else
-        psi_est[:] = 0.0
-    end
-
-    set_fac!(H, sum(scheme.A[1,:]), sum(scheme.A[1,:].*f.(t+dt*scheme.c)))
-    set_matrix_times_minus_i!(H, false) # this is done by expv
-    expv!(psi, dt, H, psi, anorm=get_norm0(H), 
-          matrix_times_minus_i=true, hermitian=true,
-          wsp=get_wsp(H), iwsp=get_iwsp(H))
-    set_matrix_times_minus_i!(H, true)
-
-    Gamma!(s, H, psi, scheme.p, dt,
-            sum(scheme.A[1,:]),
-            sum(scheme.A[1,:].*f.(t+dt*scheme.c)), 
-            symmetrized_defect?
-               sum((scheme.c-0.5).*scheme.A[1,:].*fd.(t+dt*scheme.c)):
-               sum(scheme.c.*scheme.A[1,:].*fd.(t+dt*scheme.c)),
-            s1, s2)
-    psi_est[:] += s[:]
-
-    for j=2:size(scheme.A, 1)
-
-        set_fac!(H, sum(scheme.A[j,:]), sum(scheme.A[j,:].*f.(t+dt*scheme.c)))
-        set_matrix_times_minus_i!(H, false) # this is done by expv
-        expv!(psi_est, dt, H, psi_est, anorm=get_norm0(H), 
-              matrix_times_minus_i=true, hermitian=true,
-              wsp=get_wsp(H), iwsp=get_iwsp(H)) 
-        set_matrix_times_minus_i!(H, true)
-
-        set_fac!(H, sum(scheme.A[j,:]), sum(scheme.A[j,:].*f.(t+dt*scheme.c)))
-        set_matrix_times_minus_i!(H, false) # this is done by expv
-        expv!(psi, dt, H, psi, anorm=get_norm0(H), 
-              matrix_times_minus_i=true, hermitian=true,
-              wsp=get_wsp(H), iwsp=get_iwsp(H)) 
-        set_matrix_times_minus_i!(H, true)
-    
-        Gamma!(s, H, psi, scheme.p, dt, 
-                sum(scheme.A[j,:]),
-                sum(scheme.A[j,:].*f.(t+dt*scheme.c)), 
-                symmetrized_defect?
-                   sum((scheme.c-0.5).*scheme.A[j,:].*fd.(t+dt*scheme.c)):
-                   sum(scheme.c.*scheme.A[j,:].*fd.(t+dt*scheme.c)),
-                s1, s2)
-
-        psi_est[:] += s[:]
-
-    end
-   
-    if symmetrized_defect
-        set_fac!(H, 0.5, 0.5*f(t+dt))
-    else
-        set_fac!(H, 1.0, f(t+dt))
-    end
-    LinAlg.A_mul_B!(s, H, psi)
-    psi_est[:] -= s[:]
-
-    psi_est[:] *= dt/(scheme.p+1)
-
-    restore_state!(H, state)
-end
-
-
-import Base.LinAlg: size 
-size(E::Magnus2Exponent, dim) = size(E.H, dim)
-
-import Base.LinAlg: A_mul_B!
-
-function A_mul_B!(w::Vector{Complex{Float64}}, E::Magnus2Exponent, 
-                  v::Vector{Complex{Float64}})
-    set_matrix_times_minus_i!(H, false)
-    set_fac!(E.H, 1.0, E.f)
-    A_mul_B!(w, E.H, v)
-end
-
-function step!(psi::Array{Complex{Float64},1}, H, f::Function, 
-               t::Real, dt::Real, scheme::Type{Magnus2})
-    state = save_state(H)
-
-    E = Magnus2Exponent(H, f(t+0.5*dt))
-    expv!(psi, dt, E, psi, anorm=get_norm0(H), 
-          matrix_times_minus_i=true, hermitian=true,
-          wsp=get_wsp(H), iwsp=get_iwsp(H))
-
-    restore_state!(H, state)
-end   
-
-
-abstract type Magnus4 end
-
-function get_lwsp_liwsp_expv(H, scheme::Type{Magnus4}, m::Integer=30) 
-    (lw, liw) = get_lwsp_liwsp_expv(size(H, 2), m)
-    (lw+size(H, 2), liw)
-end
-
-struct Magnus4Exponent
-    H
-    c::Complex{Float64}
-    f1::Complex{Float64}
-    f2::Complex{Float64}
-end
-
-import Base.LinAlg: size 
-size(E::Magnus4Exponent, dim) = size(E.H, dim)
-
-import Base.LinAlg: A_mul_B!
-
-function A_mul_B!(w::Vector{Complex{Float64}}, E::Magnus4Exponent, 
-                  v::Vector{Complex{Float64}})
-    set_matrix_times_minus_i!(E.H, false)
-    n = size(E.H, 2)
-    s = unsafe_wrap(Array, pointer(get_wsp(E.H), length(get_wsp(E.H))-n+1), n, false)
-
-    set_fac!(E.H, 1.0, E.f1)
-    A_mul_B!(s, E.H, v)
-
-    w[:] = 0.5*s[:]
-    
-    set_fac!(E.H, 1.0, E.f2)
-    A_mul_B!(s, E.H, s)
-    
-    w[:] -= E.c*s
-
-    set_fac!(E.H, 1.0, E.f2)
-    A_mul_B!(s, E.H, v)
-
-    w[:] += 0.5*s
-
-    set_fac!(E.H, 1.0, E.f1)
-    A_mul_B!(s, E.H, s)
-    
-    w[:] += E.c*s
-end
-
-
-function step!(psi::Array{Complex{Float64},1}, H, f::Function, 
-               t::Real, dt::Real, scheme::Type{Magnus4})
-    state = save_state(H)
-
-    E = Magnus4Exponent(H, dt*sqrt(3)/12*1im, 
-                           f(t+dt*(1/2-sqrt(3)/6)), 
-                           f(t+dt*(1/2+sqrt(3)/6)))
-    expv!(psi, dt, E, psi, anorm=get_norm0(H), 
-          matrix_times_minus_i=true, hermitian=true,
-          wsp=get_wsp(H), iwsp=get_iwsp(H))
-
-    restore_state!(H, state)
-end   
-
-
-
-get_order(::Type{Magnus4}) = 4
-
-function step_estimated!(psi::Array{Complex{Float64},1}, psi_est::Array{Complex{Float64},1},
-                 H, f::Function, fd::Function, t::Real, dt::Real,
-                 scheme::Type{Magnus4})
-    state = save_state(H)
-
-    n = size(H, 2)
-    s = unsafe_wrap(Array, pointer(get_wsp(H), 1), n, false)
-
-    step!(psi, H, f, t0, dt, Magnus4)
-   
-    ### TODO...
-
-    restore_state!(H, state)
-end
-
-
 
 
 struct EquidistantTimeStepper
@@ -591,6 +556,7 @@ struct AdaptiveTimeStepper
     dt::Float64
     tol::Float64
     order::Int
+    dt_max::Float64
     scheme
     psi_est::Array{Complex{Float64},1}
     psi0::Array{Complex{Float64},1}
@@ -598,7 +564,7 @@ struct AdaptiveTimeStepper
     function AdaptiveTimeStepper(H, 
                  f::Function, fd::Function,
                  psi::Array{Complex{Float64},1},
-                 t0::Real, tend::Real, dt::Real,  tol::Real; scheme=CF4)
+                 t0::Real, tend::Real, dt::Real,  tol::Real; scheme=CF4, dt_max=realmax(Float16))
         order = get_order(scheme)
 
         # allocate workspace
@@ -609,7 +575,7 @@ struct AdaptiveTimeStepper
         psi_est = zeros(Complex{Float64}, size(H, 2))
         psi0 = zeros(Complex{Float64}, size(H, 2))
         
-        new(H, f, fd, psi, t0, tend, dt, tol, order, scheme, psi_est, psi0)
+        new(H, f, fd, psi, t0, tend, dt, tol, order, dt_max, scheme, psi_est, psi0)
     end
     
 end
@@ -645,7 +611,7 @@ function Base.next(ats::AdaptiveTimeStepper, state::AdaptiveTimeStepperState)
         dt0 = dt
         step_estimated!(ats.psi, ats.psi_est, ats.H, ats.f, ats.fd, state.t, dt, ats.scheme)
         err = norm(ats.psi_est)/ats.tol
-        dt = dt*min(facmax, max(facmin, fac*(1.0/err)^(1.0/(ats.order+1))))
+        dt = min(ats.dt_max, dt*min(facmax, max(facmin, fac*(1.0/err)^(1.0/(ats.order+1)))))
         if err>=1.0
            ats.psi[:] = ats.psi0
            @printf("t=%17.9e  err=%17.8e  dt=%17.8e  rejected...\n", Float64(state.t), Float64(err), Float64(dt))
